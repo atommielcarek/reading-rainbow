11476f733a726897eefe64ae3ffe0e593607428d
Prometheus <atommielcarek@gmail.com>
2021-09-22 20:09:50 -0600

    updated working index.js

--- index.js
+++ index.js
@@ -1,88 +1,147 @@
-const inquirer = require("inquirer");
-const axios = require('axios').default;
-const fs = require('fs');
-const generate = require('./readmeMarkdown');
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
 
-const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What will this project be called?"
-    },
-    {
-        type: "input",
-        name: "badge",
-        message: "Enter what type(s) of badges/headings you would like."
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please provide a brief description/user story affiliated with the project."
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "How will is the application/package installed?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "How is this application/package used?"
-    },
-    {
-        type: "input",
-        name: "license",
-        message: "Provide a license or badge link for this project."
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Who are the contributing parties?"
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "Provide any project tests"
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "Please enter your Github user name."
-    },
-    {
-        type: "input",
-        name: "repo",
-        message: "Insert a link to the project's repository."
-    },
-];
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
 
-inquirer
-    .prompt(questions)
-    .then(function(data){
-        const queryUrl = `https://api.github.com/users/${data.username}`;
+// array of questions for user
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter your Github user name.",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!!!");
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter your email address!",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your email address!!!");
+                return false; 
+            }
+        }
 
-        axios.get(queryUrl).then(function(res) {
-            
-            const githubInfo = {
-                githubImage: res.data.avatar_url,
-                email: res.data.email,
-                profile: res.data.html_url,
-                name: res.data.name
-            };
-            
-          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
-            if (err) {
-              throw err;
-            };
-    
-            console.log("README file successfully created!");
-          });
-        });
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "Please enter your project name!",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter your project name!!!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please provide a brief description/user story affiliated with the project!",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please provide a brief description/user story affiliated with the project!!!");
+                return false; 
+            }
+        }
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license should your project have?",
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("What kind of license should your project have?!?!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "What are the steps required to install your project?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("What are the steps required to install your project?!?!?");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "How is this app used?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter a description!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: "What command input should be used to run tests?",
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: "What does the user need to know about contributing to the repo?"
+    }
+]);
+};
 
-});
+// function to write README file using file system 
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been created successfully !")
+        }
+    })
+}; 
 
-function init() {
-
-}
-
-init();
+// function call to initialize program
+questions()
+// getting user answers 
+.then(answers => {
+    return generatePage(answers);
+})
+// using data to display on page 
+.then(data => {
+    return writeFile(data);
+})
+// catching errors 
+.catch(err => {
+    console.log(err)
+})
